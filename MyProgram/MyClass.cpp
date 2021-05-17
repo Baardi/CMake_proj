6f07378bf0b176deb17e0a31fc163c7047b0e229
Bård Sigurd Møller <bardsigurd@gmail.com>
2021-05-17 21:26:57 +0200

    updated

---
+++ MyProgram/MyClass.cpp
@@ -0,0 +1,8 @@
+#include "MyClass.hpp"
+#include <iostream>
+#include <ranges>
+
+void MyClass::MyMethod()
+{
+    std::cout << "My test" << std::endl; 
+}
--- MyProgram/main.cpp
+++ MyProgram/main.cpp
@@ -3,6 +3,9 @@
 #include <ranges>
 #include <vector>
 #include <algorithm>
+#include "MyClass.hpp"
+#include <memory>
+#include <set>
 
 void func()
 {
@@ -10,32 +13,31 @@ void func()
     std::cout << a << " This is democracy manifest" << std::endl;
 }
 
-template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T> &vec)
-{
-    os << "{";
-    for (auto it = vec.begin(); it != vec.end(); ++it)
-    {
-        os << *it;
-        if (it != --vec.end())
-           os << ", "; 
-    }
-    return  os << "}";
-}
-
 int main()
 {
 #ifdef __MINGW64__ 
     std::cout << "x64" << std::endl;
-#endif
+#endif    
+    {
 
-    std::vector<int> v{1, 3, 4, 5};
+        //std::cout << *myNode << std::endl;
+    }
+
+    MyClass m;
+    m.MyMethod();
+
+    std::set<int> v{1, 3, 4, 5};
+    
+    auto it2 = v.find(5);
+    auto it3 = std::find(v.begin(), v.end(), 5);
+
     auto it = std::ranges::find(v, 4);
+    
     v.erase(it);
     
     [&](){
-        std::cout << "this is cpp-version" << __cplusplus << std::endl;
-        std::cout << "this is " << v << " manifest!" << std::endl;
+        std::cout << "this is cpp-version " << __cplusplus << std::endl;
+        //std::cout << "this is " << v << " manifest!" << std::endl;
     }();
     
     func();
